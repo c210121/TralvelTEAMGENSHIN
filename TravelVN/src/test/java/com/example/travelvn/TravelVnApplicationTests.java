8fcc8346098e370b0fc6ec2a68bf53b94151179c
user <c210121@ccg.ac.jp>
2022-09-20 18:22:00 +0900

    Update thay đổi (thêm)

---
+++ TravelVN/src/test/java/com/example/travelvn/TravelVnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelVnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
